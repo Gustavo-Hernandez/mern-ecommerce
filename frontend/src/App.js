adadea072afaa1f900dc1d9f2f28f9f31cc513f5
Gustavo Hernández <47768780+Gustavo-Hernandez@users.noreply.github.com>
2021-02-02 10:49:55 -0600

    Added Product and Rating components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to MERN eCommerce</h1>
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
