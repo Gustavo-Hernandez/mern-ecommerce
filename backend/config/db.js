17cd123ebb51d1af42d51ae5a0984bfc0e54fa8c
Gustavo Hernández <47768780+Gustavo-Hernandez@users.noreply.github.com>
2021-02-03 12:01:09 -0600

    Added colors package for console debug.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import colors from 'colors';
 
 const connectDB = async () => {
   try {
@@ -7,9 +8,11 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
     });
-    console.log(`MongoDB Connected ${connector.connection.host}`);
+    console.log(
+      `[MongoDB] Connected ${connector.connection.host}`.green.underline
+    );
   } catch (error) {
-    console.log(`[ERROR | MongoDB] ${error.message}`);
+    console.log(`[ERROR | MongoDB] ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
